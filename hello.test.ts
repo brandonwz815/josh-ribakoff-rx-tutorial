168a0b99d9471bad0d7253923ca96cf4137dfd75
Brandon Zhu <bzhu@utraque.com>
2021-09-12 18:56:04 -0400

    Part 2

--- hello.test.ts
+++ hello.test.ts
@@ -1,3 +1,20 @@
+import { combineLatest, merge, Subject } from 'rxjs';
+
+
 it('works', () => {
-  expect(true).toBe(true);
-})
+  const a = new Subject<number>();
+  const b = new Subject<number>();
+  // const c = merge([a, b]);
+  const c = combineLatest([a, b]);
+
+  // consumer
+  // c.subscribe(console.log);
+  c.subscribe(([a, b]) => console.log(a + b));
+
+  // producer
+  // a.next('hello world');
+  // b.next('hello world 2');
+  a.next(3);
+  b.next(3);
+  b.next(4);
+});
